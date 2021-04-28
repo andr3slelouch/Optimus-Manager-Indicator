6a18c83cfcadafaf24dad5cc0f4d4e4b93be9495
andr3slelouch <luis.andradec14@gmail.com>
2021-04-27 23:24:16 -0500

    Refactored for following sonarcloud recomendations

--- prefs.js
+++ prefs.js
@@ -3,17 +3,19 @@ const Gtk = imports.gi.Gtk;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Config = imports.misc.config;
-const [major] = Config.PACKAGE_VERSION.split('.');
+const [major] = Config.PACKAGE_VERSION.split(".");
 const shellVersion = Number.parseInt(major);
 const Gettext = imports.gettext;
 
-Gettext.bindtextdomain( "OptimusManagerIndicator", Me.dir.get_child('locale').get_path() );
+Gettext.bindtextdomain(
+  "OptimusManagerIndicator",
+  Me.dir.get_child("locale").get_path()
+);
 Gettext.textdomain("OptimusManagerIndicator");
 
 const _ = Gettext.gettext;
 
-function init() {
-}
+function init() {}
 
 function buildPrefsWidget() {
   let settings = ExtensionUtils.getSettings(
@@ -24,21 +26,25 @@ function buildPrefsWidget() {
     row_spacing: 12,
     halign: Gtk.Align.CENTER,
   });
-  if (shellVersion < 40){
-    let grid = new Gtk.Grid({
+  if (shellVersion < 40) {
+    grid = new Gtk.Grid({
       margin: 12,
       column_spacing: 12,
       row_spacing: 12,
       halign: Gtk.Align.CENTER,
-    });  
+    });
   }
-  
+
   let alwaysGpuTempLbl = new Gtk.Label({
-    label: _("Always show GPU Temperature, even if Optimus Manager is unavailable"),
+    label: _(
+      "Always show GPU Temperature, even if Optimus Manager is unavailable"
+    ),
     halign: Gtk.Align.START,
   });
   let dynamicHybridLbl = new Gtk.Label({
-    label: _("Dynamic icon behaviour in hybrid mode, nvidia icon only if gpu is in use"),
+    label: _(
+      "Dynamic icon behaviour in hybrid mode, nvidia icon only if gpu is in use"
+    ),
     halign: Gtk.Align.START,
   });
   let showGPUTempLbl = new Gtk.Label({
@@ -54,7 +60,10 @@ function buildPrefsWidget() {
     halign: Gtk.Align.START,
   });
   let restartGShellLbl = new Gtk.Label({
-    label: _("Restart Gnome Shell ")+shellVersion.toString() +_(" (alt + F2, type r and hit ENTER) to see the changes"),
+    label:
+      _("Restart Gnome Shell ") +
+      shellVersion.toString() +
+      _(" (alt + F2, type r and hit ENTER) to see the changes"),
     halign: Gtk.Align.START,
   });
 
@@ -68,7 +77,7 @@ function buildPrefsWidget() {
     label: _(" Save Changes "),
     visible: true,
   });
-  
+
   grid.attach(alwaysGpuTempLbl, 0, 0, 1, 1);
   grid.attach(dynamicHybridLbl, 0, 1, 1, 1);
   grid.attach(showGPUTempLbl, 0, 2, 1, 1);
@@ -87,16 +96,24 @@ function buildPrefsWidget() {
   dynamicHybridSw.set_state(settings.get_boolean("dynamic-hybrid-icon"));
   showGPUTempSw.set_state(settings.get_boolean("show-gpu-temperature"));
   showGPUUtilizationSw.set_state(settings.get_boolean("show-gpu-utilization"));
-  showGPUMemoryUtilizationSw.set_state(settings.get_boolean("show-gpu-memory-utilization"));
+  showGPUMemoryUtilizationSw.set_state(
+    settings.get_boolean("show-gpu-memory-utilization")
+  );
 
   saveButton.connect("clicked", (button) => {
     settings.set_boolean("forced-mode", alwaysGpuTempSw.get_state());
     settings.set_boolean("dynamic-hybrid-icon", dynamicHybridSw.get_state());
-    settings.set_boolean("show-gpu-temperature",showGPUTempSw.get_state());
-    settings.set_boolean("show-gpu-utilization",showGPUUtilizationSw.get_state());
-    settings.set_boolean("show-gpu-memory-utilization",showGPUMemoryUtilizationSw.get_state());
+    settings.set_boolean("show-gpu-temperature", showGPUTempSw.get_state());
+    settings.set_boolean(
+      "show-gpu-utilization",
+      showGPUUtilizationSw.get_state()
+    );
+    settings.set_boolean(
+      "show-gpu-memory-utilization",
+      showGPUMemoryUtilizationSw.get_state()
+    );
   });
-  if (shellVersion < 40){
+  if (shellVersion < 40) {
     grid.show_all();
   }
   return grid;
--- prime.js
+++ prime.js
@@ -2,7 +2,7 @@
 /* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
 
 // strict mode
-'use strict';
+"use strict";
 
 // import modules
 const GLib = imports.gi.GLib;
@@ -19,211 +19,208 @@ const Me = ExtensionUtils.getCurrentExtension();
  * @return {Object}
  */
 var Switch = class Switch {
+  /**
+   * Constructor
+   *
+   * @return {Void}
+   */
+  constructor() {
+    this._commands = null;
+    this._gpu = null;
+    this._listener = null;
 
-    /**
-     * Constructor
-     *
-     * @return {Void}
-     */
-    constructor() {
-        this._commands = null;
-        this._gpu = null;
-        this._listener = null;
+    this._commands = {
+      sudo: this._which("pkexec") || this._which("gksudo"),
+      select: this._which("prime-select"),
+      management: this._which("nvidia-smi"),
+      settings: this._which("nvidia-settings"),
+    };
 
-        this._commands = {
-            sudo: this._which('pkexec') || this._which('gksudo'),
-            select: this._which('prime-select'),
-            management: this._which('nvidia-smi'),
-            settings: this._which('nvidia-settings'),
-        }
+    // make getter store default query
+    // (if not already done so)
+    this.defaultQuery = null;
+  }
 
-        // make getter store default query
-        // (if not already done so)
-        this.defaultQuery;
+  /**
+   * Destructor
+   *
+   * @return {Void}
+   */
+  destroy() {
+    this.unmonitor();
+  }
+
+  /**
+   * `which $command` result
+   *
+   * @param  {String} command
+   * @return {Mixed}
+   */
+  _which(command) {
+    let exec = this._shellExec("which " + command);
+    return exec.stdout.trim() || exec.stderr.trim();
+  }
+
+  /**
+   * Shell execute command
+   *
+   * @param  {String} command
+   * @return {Object}
+   */
+  _shellExec(command) {
+    let result = {
+      status: -1,
+      stdin: command,
+      stdout: "",
+      stderr: "",
+    };
+
+    try {
+      let subprocess = new Gio.Subprocess({
+        argv: command.split(" "),
+        flags:
+          Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+      });
+      subprocess.init(null);
+
+      let [, stdout, stderr] = subprocess.communicate_utf8(null, null);
+      result.status = subprocess.get_exit_status();
+      result.stdout = stdout;
+      result.stderr = stderr;
+    } catch (e) {
+      result.stderr = e.toString();
     }
 
-    /**
-     * Destructor
-     *
-     * @return {Void}
-     */
-    destroy() {
-        this.unmonitor();
-    }
+    return result;
+  }
 
-    /**
-     * `which $command` result
-     *
-     * @param  {String} command
-     * @return {Mixed}
-     */
-    _which(command) {
-        let exec = this._shellExec('which ' + command);
-        return exec.stdout.trim() || exec.stderr.trim();
-    }
+  /**
+   * Shell execute command
+   *
+   * @param  {String}   command
+   * @param  {Function} callback (optional)
+   * @return {Void}
+   */
+  _shellExecAsync(command, callback) {
+    try {
+      let subprocess = new Gio.Subprocess({
+        argv: command.split(" "),
+        flags:
+          Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+      });
 
-    /**
-     * Shell execute command
-     *
-     * @param  {String} command
-     * @return {Object}
-     */
-    _shellExec(command) {
-        let result = {
-            status: -1,
-            stdin: command,
-            stdout: '',
-            stderr: '',
-        }
+      subprocess.init(null);
+      subprocess.communicate_utf8_async(
+        null,
+        null,
+        function (source, resource) {
+          let status = source.get_exit_status(),
+            [, stdout, stderr] = source.communicate_utf8_finish(resource);
 
-        try {
-            let subprocess = new Gio.Subprocess({
-                argv: command.split(' '),
-                flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+          if (typeof callback === "function")
+            callback.call(this, {
+              status: status,
+              stdin: command,
+              stdout: stdout,
+              stderr: stderr,
             });
-            subprocess.init(null);
+        }.bind(this)
+      );
+    } catch (e) {
+      if (typeof callback === "function")
+        callback.call(this, {
+          status: -1,
+          stdin: command,
+          stdout: "",
+          stderr: e.toString(),
+        });
+    }
+  }
 
-            let [, stdout, stderr] = subprocess.communicate_utf8(null, null);
-            result.status = subprocess.get_exit_status();;
-            result.stdout = stdout;
-            result.stderr = stderr;
-        }
-        catch(e) {
-            result.stderr = e.toString();
-        }
+  /**
+   * File with prime status
+   *
+   * @type {String}
+   */
+  get index() {
+    return "/etc/prime-discrete";
+  }
 
-        return result;
+  /**
+   * Property gpu getter:
+   * if `nvidia-smi -q` shell command exit code
+   * is non-zero, 'nvidia' is not in use
+   *
+   * @return {String}
+   */
+  get gpu() {
+    if (this._gpu) return this._gpu;
+
+    let cmd = this.command("management");
+    if (cmd) {
+      let exec = this._shellExec(cmd + " -L");
+      this._gpu = exec.status ? "intel" : "nvidia";
+    } else this._gpu = "unknown";
+
+    return this.gpu;
+  }
+
+  /**
+   * Property query getter:
+   * shell command `prime-select query` result
+   *
+   * @return {String}
+   */
+  get query() {
+    let cmd = this.command("select");
+    if (cmd) {
+      let exec = this._shellExec(cmd + " query");
+      return exec.stdout.trim() || exec.stderr.trim() || "unknown";
     }
 
-    /**
-     * Shell execute command
-     *
-     * @param  {String}   command
-     * @param  {Function} callback (optional)
-     * @return {Void}
-     */
-    _shellExecAsync(command, callback) {
-        try {
-            let subprocess = new Gio.Subprocess({
-                argv: command.split(' '),
-                flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
-            });
+    return "unknown";
+  }
 
-            subprocess.init(null);
-            subprocess.communicate_utf8_async(null, null, function(source, resource) {
-                let status = source.get_exit_status(),
-                    [, stdout, stderr] = source.communicate_utf8_finish(resource);
+  /**
+   * Property default query getter:
+   * query on system init
+   *
+   * @return {String}
+   */
+  get defaultQuery() {
+    let result = GLib.getenv("PRIME_INDICATOR_DEFAULT_QUERY");
+    if (result) return result;
 
-                if (typeof callback === 'function')
-                    callback.call(this, {
-                        status: status,
-                        stdin: command,
-                        stdout: stdout,
-                        stderr: stderr,
-                    });
-            }.bind(this));
-        }
-        catch(e) {
-            if (typeof callback === 'function')
-                callback.call(this, {
-                    status: -1,
-                    stdin: command,
-                    stdout: '',
-                    stderr: e.toString(),
-                });
-        }
-    }
+    result = this.query;
+    GLib.setenv("PRIME_INDICATOR_DEFAULT_QUERY", result, true);
+    log("detected " + result + " as default prime option");
 
-    /**
-     * File with prime status
-     *
-     * @type {String}
-     */
-    get index() {
-        return '/etc/prime-discrete';
-    }
+    return this.defaultQuery;
+  }
 
-    /**
-     * Property gpu getter:
-     * if `nvidia-smi -q` shell command exit code
-     * is non-zero, 'nvidia' is not in use
-     *
-     * @return {String}
-     */
-    get gpu() {
-        if (this._gpu)
-            return this._gpu;
+  /**
+   * Get switches (valid arguments for
+   * switch command)
+   *
+   * @return {Array}
+   */
+  get switches() {
+    if (this._switches) return this._switches.slice();
 
-        let cmd = this.command('management');
-        if (cmd) {
-            let exec = this._shellExec(cmd + ' -L');
-            this._gpu = exec.status ? 'intel' : 'nvidia';
-        }
-        else
-            this._gpu = 'unknown';
+    let command = this.command("select"),
+      exec = this._shellExec(command),
+      output = exec.stdout || exec.stderr,
+      args = output.trim().split(" ").pop().split("|");
+    if (args.length)
+      this._switches = args.filter((item) => {
+        return item !== "query";
+      });
+    else this._switches = ["nvidia", "intel"];
 
-        return this.gpu;
-    }
+    return this.switches;
+  }
 
-    /**
-     * Property query getter:
-     * shell command `prime-select query` result
-     *
-     * @return {String}
-     */
-    get query() {
-        let cmd = this.command('select');
-        if (cmd) {
-            let exec = this._shellExec(cmd + ' query');
-            return exec.stdout.trim() || exec.stderr.trim() || 'unknown';
-        }
-
-        return 'unknown';
-    }
-
-    /**
-     * Property default query getter:
-     * query on system init
-     *
-     * @return {String}
-     */
-    get defaultQuery() {
-        let result = GLib.getenv('PRIME_INDICATOR_DEFAULT_QUERY');
-        if (result)
-            return result;
-
-        result = this.query;
-        GLib.setenv('PRIME_INDICATOR_DEFAULT_QUERY', result, true);
-        log('detected ' + result + ' as default prime option');
-
-        return this.defaultQuery;
-    }
-
-    /**
-     * Get switches (valid arguments for
-     * switch command)
-     *
-     * @return {Array}
-     */
-    get switches() {
-        if (this._switches)
-            return this._switches.slice();;
-
-        let command = this.command('select'),
-            exec = this._shellExec(command),
-            output = exec.stdout || exec.stderr,
-            args = output.trim().split(' ').pop().split('|');
-        if (args.length)
-            this._switches = args.filter((item) => {
-                return item !== 'query';
-            });
-        else
-            this._switches = [ 'nvidia', 'intel' ];
-
-        return this.switches;
-    }
-
-    /**
+  /**
      * Does sysem need restarting:
      * we store query value on initialization,
      * and if this value differs from current
@@ -238,119 +235,112 @@ var Switch = class Switch {
      *
      * @return {Boolean}
      */
-    get needsRestart() {
-        return this.query !== this.defaultQuery && this.command('select');
-    }
+  get needsRestart() {
+    return this.query !== this.defaultQuery && this.command("select");
+  }
 
-    /**
-     * Get shell command
-     *
-     * @param  {String} cmd sudo|select|management|settings
-     * @return {String}     null on fail
-     */
-    command(cmd) {
-        if (cmd in this._commands)
-            return this._commands[cmd];
+  /**
+   * Get shell command
+   *
+   * @param  {String} cmd sudo|select|management|settings
+   * @return {String}     null on fail
+   */
+  command(cmd) {
+    if (cmd in this._commands) return this._commands[cmd];
 
-        return null;
-    }
+    return null;
+  }
 
-    /**
-     * GPU switch
-     * shell command `prime-select $gpu`, where
-     * gpu is 'intel' or 'nvidia'
-     *
-     * @param  {String}   gpu    intel|nvidia
-     * @param  {Function} logout (optional)
-     * @return {Void}
-     */
-    switch(gpu, callback) {
-        let sudo = this.command('sudo');
-        if (!sudo)
-            return;
+  /**
+   * GPU switch
+   * shell command `prime-select $gpu`, where
+   * gpu is 'intel' or 'nvidia'
+   *
+   * @param  {String}   gpu    intel|nvidia
+   * @param  {Function} logout (optional)
+   * @return {Void}
+   */
+  switch(gpu, callback) {
+    let sudo = this.command("sudo");
+    if (!sudo) return;
 
-        let select = this.command('select');
-        if (!select)
-            return;
+    let select = this.command("select");
+    if (!select) return;
 
-        if (this.query === gpu)
-            return;
+    if (this.query === gpu) return;
 
-        let cmd = sudo
-             + ' ' + select
-             + ' ' + gpu
+    let cmd = sudo + " " + select + " " + gpu;
 
-        log('switching to ' + gpu);
-        this._shellExecAsync(cmd, function(e) {
-            if (!e.status)
-                log('switched to ' + gpu);
-            else
-                log('not switched to ' + gpu + ' (' + e.stderr.trim() + ')');
+    log("switching to " + gpu);
+    this._shellExecAsync(
+      cmd,
+      function (e) {
+        if (!e.status) log("switched to " + gpu);
+        else log("not switched to " + gpu + " (" + e.stderr.trim() + ")");
 
-            if (!e.status && this.needsRestart)
-                log('system restart required');
+        if (!e.status && this.needsRestart) log("system restart required");
 
-            if (typeof callback === 'function')
-                callback.call(this, {
-                    gpu: gpu,
-                    result: !e.status,
-                });
-        }.bind(this));
-    }
+        if (typeof callback === "function")
+          callback.call(this, {
+            gpu: gpu,
+            result: !e.status,
+          });
+      }.bind(this)
+    );
+  }
 
-    /**
-     * Start nvidia-settings
-     *
-     * @return {Void}
-     */
-    settings() {
-        let cmd = this.command('settings');
-        if (!cmd)
-            return;
+  /**
+   * Start nvidia-settings
+   *
+   * @return {Void}
+   */
+  settings() {
+    let cmd = this.command("settings");
+    if (!cmd) return;
 
-        this._shellExecAsync(cmd);
-    }
+    this._shellExecAsync(cmd);
+  }
 
-    /**
-     * Start file monitoring
-     *
-     * @return {Void}
-     */
-    monitor() {
-        if (this._listener)
-            return;
+  /**
+   * Start file monitoring
+   *
+   * @return {Void}
+   */
+  monitor() {
+    if (this._listener) return;
 
-        this._listener = Gio.File.new_for_path(this.index).monitor_file(Gio.FileMonitorFlags.NONE, null);
-        this._listener.connect('changed', this._handleListener.bind(this));
-    }
+    this._listener = Gio.File.new_for_path(this.index).monitor_file(
+      Gio.FileMonitorFlags.NONE,
+      null
+    );
+    this._listener.connect("changed", this._handleListener.bind(this));
+  }
 
-    /**
-     * Stop file monitoring
-     *
-     * @return {Void}
-     */
-    unmonitor() {
-        if (!this._listener)
-            return;
+  /**
+   * Stop file monitoring
+   *
+   * @return {Void}
+   */
+  unmonitor() {
+    if (!this._listener) return;
 
-        this._listener.cancel();
-        this._listener = null;
-    }
+    this._listener.cancel();
+    this._listener = null;
+  }
 
-    /**
-     * File monitor change event handler
-     *
-     * @param  {Object} file
-     * @param  {Object} otherFile
-     * @param  {Object} eventType
-     * @return {Void}
-     */
-    _handleListener(file, otherFile, eventType) {
-        this.emit('gpu-change', this.query);
-    }
+  /**
+   * File monitor change event handler
+   *
+   * @param  {Object} file
+   * @param  {Object} otherFile
+   * @param  {Object} eventType
+   * @return {Void}
+   */
+  _handleListener(file, otherFile, eventType) {
+    this.emit("gpu-change", this.query);
+  }
 
-    /* --- */
-
+  /* --- */
 };
 
 Signals.addSignalMethods(Switch.prototype);
